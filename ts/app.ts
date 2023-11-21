1d4805689ab263ccaa9a0db7acad20a0c2fa94c2
Rafael Molina Osias <engineer.raflash@gmail.com>
2023-11-21 14:37:55 -0500

    se Oculta  Alerta si pasa la validación

--- ts/app.ts
+++ ts/app.ts
@@ -15,21 +15,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let inputEmail: HTMLInputElement;
     let inputAsunto: HTMLInputElement;
     let inputMensaje: HTMLTextAreaElement;
-    let formulario: HTMLFormElement;
 
     const seleccionarElementosInterface = (): void => {
         inputEmail = <HTMLInputElement>document.getElementById('email');
         inputAsunto = <HTMLInputElement>document.getElementById('asunto');
         inputMensaje = <HTMLTextAreaElement>document.getElementById('mensaje');
-        formulario = <HTMLFormElement>document.getElementById('formulario');
     };
 
     const mostrarAlerta = (mensage:string, referencia:HTMLElement): void => {
 
-        const alerta = referencia.querySelector('.bg-red-600');
-        if (alerta) {
-            alerta.remove();
-        }
+        limpiarAlerta(referencia);
 
         const error = document.createElement('P');
         error.classList.add(
@@ -43,14 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     };
 
+    const limpiarAlerta =(referencia:HTMLElement):void =>{
+        const alerta = referencia.querySelector('.bg-red-600');
+        if (alerta) {
+            alerta.remove();
+        }
+    }
+
     const validar = (evt: Event): void => {
         evt.preventDefault();
         const targetElement = <HTMLInputElement|HTMLTextAreaElement>  evt.target;
-        console.log(targetElement);
         if (targetElement.value.trim() === '') {
             mostrarAlerta(`El campo ${targetElement.id} es obligatorio`,targetElement.parentElement!);
+            return;
         }
-        // else {console.log((evt.target as HTMLInputElement).value);}
+        limpiarAlerta(targetElement.parentElement!);
     };
 
     const asignarEventos = (): void => {
