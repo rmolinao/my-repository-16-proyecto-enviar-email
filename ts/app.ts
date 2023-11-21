993eeff7014c3f886e155a4ba22ca389ae13b99c
Rafael Molina Osias <engineer.raflash@gmail.com>
2023-11-21 14:00:28 -0500

    Creando alertas de error, que validan los campos y se muestran junto a estos, previniendo que se genere muchas veces

--- ts/app.ts
+++ ts/app.ts
@@ -12,28 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
         mensaje: ''
     }
 
-    let inputEmail:HTMLInputElement;
-    let inputAsunto:HTMLInputElement;
-    let inputMensaje:HTMLTextAreaElement;
+    let inputEmail: HTMLInputElement;
+    let inputAsunto: HTMLInputElement;
+    let inputMensaje: HTMLTextAreaElement;
+    let formulario: HTMLFormElement;
 
     const seleccionarElementosInterface = (): void => {
-        inputEmail = <HTMLInputElement> document.getElementById('email') ;
-        inputAsunto = <HTMLInputElement> document.getElementById('asunto')  ;
-        inputMensaje = <HTMLTextAreaElement> document.getElementById('mensaje');
+        inputEmail = <HTMLInputElement>document.getElementById('email');
+        inputAsunto = <HTMLInputElement>document.getElementById('asunto');
+        inputMensaje = <HTMLTextAreaElement>document.getElementById('mensaje');
+        formulario = <HTMLFormElement>document.getElementById('formulario');
     };
 
-    const validar = (evt: Event):void =>{
-        evt.preventDefault();
-        if ((evt.target as HTMLInputElement).value.trim() === '') {
-            console.log('esta vacio');
+    const mostrarAlerta = (mensage:string, referencia:HTMLElement): void => {
+
+        const alerta = referencia.querySelector('.bg-red-600');
+        if (alerta) {
+            alerta.remove();
         }
-        console.log((evt.target as HTMLInputElement).value);
+
+        const error = document.createElement('P');
+        error.classList.add(
+            'bg-red-600',
+            'text-white',
+            'p-2',
+            'text-center'
+        );
+        error.textContent = mensage;
+        referencia.appendChild(error);
+
+    };
+
+    const validar = (evt: Event): void => {
+        evt.preventDefault();
+        const targetElement = <HTMLInputElement|HTMLTextAreaElement>  evt.target;
+        console.log(targetElement);
+        if (targetElement.value.trim() === '') {
+            mostrarAlerta(`El campo ${targetElement.id} es obligatorio`,targetElement.parentElement!);
+        }
+        // else {console.log((evt.target as HTMLInputElement).value);}
     };
 
     const asignarEventos = (): void => {
-        inputEmail.addEventListener('blur',validar);
-        inputAsunto.addEventListener('blur',validar);
-        inputMensaje.addEventListener('blur',validar);
+        inputEmail.addEventListener('blur', validar);
+        inputAsunto.addEventListener('blur', validar);
+        inputMensaje.addEventListener('blur', validar);
     };
 
     seleccionarElementosInterface();
